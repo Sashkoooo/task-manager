78cec4cfa0bf15d2d8b57a37985d2a91b1ec3141
Sashkoooo <o.denysenko@hotmail.com>
2023-06-23 18:39:14 +0300

    Add models to admin page

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from tasks.models import (
+    Worker,
+    Position,
+    TaskType,
+    Task,
+    Team,
+    Project
+)
+
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    pass
+
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(TaskType)
+class TaskTypeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    pass
