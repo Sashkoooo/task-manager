3f0c0113f9dc1900fd199ccdee9e71fcb8f7d0a1
Sashkoooo <o.denysenko@hotmail.com>
2023-06-26 20:27:09 +0300

    add path RedirectView.as_view

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -18,9 +18,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
                   path("admin/", admin.site.urls),
-                  path("", include("tasks.urls", namespace="tasks")),
+                  path("tasks/", include("tasks.urls.urls")),
                   path("accounts/", include("django.contrib.auth.urls")),
+                  path("", RedirectView.as_view(url="/tasks/", permanent=True))
+
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
