5529e9cc285b6c12b5b15107b5fcfdaaea2f2e73
Sashkoooo <o.denysenko@hotmail.com>
2023-06-26 19:31:43 +0300

    Add LOGIN_REDIRECT_URL

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -102,6 +102,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTH_USER_MODEL = "tasks.Worker"
 
+LOGIN_REDIRECT_URL = "/tasks/"
+
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
