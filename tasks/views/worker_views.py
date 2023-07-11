5b9bad7ec45d2025df5fb004da04d8766349daa0
Sashkoooo <o.denysenko@hotmail.com>
2023-07-12 00:31:18 +0300

    Override get_context_data method to count completed_tasks, tasks_in_progress, overdue_tasks in template. Override get_queryset method to exclude superuser from workers list

--- tasks/views/worker_views.py
+++ tasks/views/worker_views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
+from django.utils import timezone
 from django.views import generic
 
 from tasks.models import Worker
@@ -9,6 +10,25 @@ class WorkerListView(LoginRequiredMixin, generic.ListView):
     model = Worker
     template_name = "tasks/worker/worker_list.html"
     paginate_by = 10
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        now = timezone.now()
+        for worker in context["object_list"]:
+            worker.completed_tasks_count = (
+                worker.tasks.filter(completed=True).count()
+            )
+            worker.tasks_in_progress_count = (
+                worker.tasks.filter(completed=False).count()
+            )
+            worker.overdue_tasks_count = (
+                worker.tasks.filter(completed=False, deadline__lt=now).count()
+            )
+        return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.exclude(is_superuser=True)
 
 
 class WorkerDetailView(LoginRequiredMixin, generic.DetailView):
