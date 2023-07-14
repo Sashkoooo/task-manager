b252543bc779087ae403c9fe47df6de138f2218c
Sashkoooo <o.denysenko@hotmail.com>
2023-07-14 23:25:38 +0300

    Delete unnecessary path functions in urlpatterns

--- tasks/urls/urls.py
+++ tasks/urls/urls.py
@@ -4,12 +4,8 @@ from tasks.views.index_views import index
 
 urlpatterns = [
     path("", index, name="index"),
-    path("positions/", include("tasks.urls.position_urls")),
     path("workers/", include("tasks.urls.worker_urls")),
-    path("task-types/", include("tasks.urls.task_type_urls")),
     path("tasks/", include("tasks.urls.task_urls")),
-    path("teams/", include("tasks.urls.team_urls")),
-    path("projects/", include("tasks.urls.project_urls")),
 ]
 
 app_name = "tasks"
--- tasks/urls/worker_urls.py
+++ tasks/urls/worker_urls.py
@@ -2,15 +2,9 @@ from django.urls import path
 from tasks.views.worker_views import (
     WorkerListView,
     WorkerDetailView,
-    WorkerCreateView,
-    WorkerUpdateView,
-    WorkerDeleteView,
 )
 
 urlpatterns = [
     path("", WorkerListView.as_view(), name="worker-list"),
-    path("<int:pk>/", WorkerDetailView.as_view(), name="worker-detail"),
-    path("create/", WorkerCreateView.as_view(), name="worker-create"),
-    path("<int:pk>/update/", WorkerUpdateView.as_view(), name="worker-update"),
-    path("<int:pk>/delete/", WorkerDeleteView.as_view(), name="worker-delete"),
+    path("tasks/<int:pk>/", WorkerDetailView.as_view(), name="worker-detail"),
 ]
