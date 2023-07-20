fef47be47bd0a46c5434e203ccf5df2245d0d2f5
Sashkoooo <o.denysenko@hotmail.com>
2023-07-20 20:28:39 +0300

    Add search by name for TaskListView by override get_context_data & get_queryset functions

--- tasks/views/task_views.py
+++ tasks/views/task_views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views import generic
 
-from tasks.forms import TaskForm
+from tasks.forms import TaskForm, TaskSearchForm
 from tasks.models import Task
 
 
@@ -10,6 +10,25 @@ class TaskListView(LoginRequiredMixin, generic.ListView):
     model = Task
     template_name = "tasks/task/task_list.html"
     paginate_by = 5
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(TaskListView, self).get_context_data(**kwargs)
+        name = self.request.GET.get("name", "")
+        context["search_form"] = TaskSearchForm(initial={
+            "name": name
+        })
+
+        return context
+
+    def get_queryset(self):
+        queryset = Task.objects.all()
+        form = TaskSearchForm(self.request.GET)
+
+        if form.is_valid():
+            return queryset.filter(
+                name__icontains=form.cleaned_data["name"]
+            )
+        return queryset
 
 
 class TaskDetailView(LoginRequiredMixin, generic.DetailView):
